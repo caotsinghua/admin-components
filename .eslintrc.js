e9e4f99a9cb32216a06c78bb38c8677e1d08bc66
caotsinghua <1577499543@qq.com>
2019-08-05 18:29:45 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', 'plugin:prettier/recommended'],
+    rules: {
+      'prettier/prettier': 'error',
+      // allow async-await
+      'generator-star-spacing': 'off',
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'vue/no-parsing-error': [
+          2,
+          {
+              'x-invalid-end-tag': false
+          }
+      ],
+      'no-undef': 'off',
+      camelcase: 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  presets: ["@vue/app"]
+module.exports = api => {
+    return {
+        presets: [
+            [
+                '@babel/preset-env',
+                {
+                    modules: 'commonjs',
+                    targets: {
+                        node: 'current'
+                    }
+                }
+            ],
+            '@vue/app'
+        ],
+        plugins: [...((api.env('test') && 'require-context-hook') || []), '@babel/plugin-syntax-dynamic-import']
+    };
 };
